ddd61538a6f999c9896bfcf124e52527e2776ff3
Hippolyte Morillon <HMorillon@schuco-usa.com>
2022-06-21 15:38:07 -0400

    fix : authentication for net core 6.0

--- AngularApp/src/app/app-core/services/auth.service.ts
+++ AngularApp/src/app/app-core/services/auth.service.ts
@@ -91,8 +91,18 @@ export class AuthService {
   }
 
   login(username: string, password: string): Observable<boolean> {
-    const requestBody = 'grant_type=password&username=' + username + '&password=' + password;
-    return this.http.post<any>(this.appConstantService.APP_DOMAIN + "Token", requestBody)
+    const requestBody = //'grant_type=password&username=' + username + '&password=' + password;
+    {
+      UserName: username,
+      Password: password,
+      Language: "en-US"
+    };
+    return this.http.post<any>(this.appConstantService.APP_DOMAIN + "api/Account/login", {
+      UserName: username,
+      Password: password,
+      Language: "en-US",
+      User: null
+    })
     .pipe(map(data => {
         this.setAuthorizationToken(data);
         // this.setPCToken().pipe(first()).subscribe((result: any)=>{
